c56f0bb7d62c661cc47f29a287ff96f4f4d65f19
zhangyoubo <13818956024@163.com>
2017-05-18 09:12:19 +0800

    2017.5.18

--- RSA 加解密/ViewController.m
+++ RSA 加解密/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "RSAEncryptor.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+    //原始数据
+    NSString *originalString = @"zhangyoubo";
+    
+    //使用.der和.p12中的公钥私钥加密解密
+    NSString *public_key_path = [[NSBundle mainBundle] pathForResource:@"public_key.der" ofType:nil];
+    NSString *private_key_path = [[NSBundle mainBundle] pathForResource:@"private_key.p12" ofType:nil];
+    
+    
+    RSAEncryptor *rsaEncryptor = [RSAEncryptor sharedInstance];
+    
+    BOOL hasPublicKey = [rsaEncryptor getPublicKeyRefrenceFromeData:[[NSData alloc] initWithContentsOfFile:public_key_path]];
+    BOOL hasPrivateKey = [rsaEncryptor getPrivateKeyRefrenceFromData:[[NSData alloc] initWithContentsOfFile:public_key_path] password:@"123456"];
+    
+    if (!hasPublicKey||!hasPrivateKey) {
+        
+        [rsaEncryptor loadPublicKeyFromFile:public_key_path];
+        [rsaEncryptor loadPrivateKeyFromFile:private_key_path password:@"123456"];
+        
+   }else{
+        
+    }
+    
+    NSString *encryptStr = [rsaEncryptor rsaEncryptString:originalString];
+    NSString *decryptStr = [rsaEncryptor rsaDecryptString:encryptStr];
+    
+    NSLog(@"加密前:%@", originalString);
+    NSLog(@"加密后:%@", encryptStr);
+    NSLog(@"解密后:%@", decryptStr);
+    
+    
+  }
 
 
 - (void)didReceiveMemoryWarning {
